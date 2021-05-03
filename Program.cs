b280bcb012187e9eeed4b0934cc9bcb082168ee2
davi santana araujo <davisantana48@gmail.com>
2021-05-03 11:15:53 -0300

    commit 2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Dio.Series
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! ");
         }
     }
 }
